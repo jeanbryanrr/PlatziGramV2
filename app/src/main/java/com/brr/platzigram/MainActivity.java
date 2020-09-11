fcfc2b4490583fac17b3dfa9f9e5df8d5d9218aa
Jean Bryan Reategui <bryan.rr@hotmail.com>
2020-09-11 15:12:48 -0500

    Initial commit

---
+++ app/src/main/java/com/brr/platzigram/MainActivity.java
@@ -0,0 +1,20 @@
+package com.brr.platzigram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goCreateAccount(View view) {
+        Log.d("XXX","GAAAAAAA");
+    }
+}
